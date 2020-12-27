56050a90bd88ca94a138dd8cc47202a0c21b8039
John Jegede <jegede400@gmail.com>
2020-12-27 16:46:15 -0500

    added functions for weather app

--- script.js
+++ script.js
@@ -3,33 +3,50 @@ $(document).ready(function () {
   var latitude;
   var longitude;
   var cities = [];
+  var cityEl = localStorage.getItem("cityEl");
+  var date = moment().format("dddd, MMMM Do YYYY");
 
   var submit = $(".btn");
+  var $citylist = $("#search");
+
+  if(cityEl !== null){
+    $("#citys").text(cityEl);
+    $("#date").text(" " + date);
+    getCordinats(cityEl);
+  }
 
   submit.on("click", function (e) {
-    //e.prevendefault();
+    e.preventDefault();
     var city = $("#input").val();
     $("#citys").text(city);
-    var date = moment().format("dddd, MMMM Do YYYY");
     $("#date").text(" " + date);
+    localStorage.setItem("cityEl",city);
 
     if (cities.includes(city) === false) {
       cities.push(city);
-      console.log(cities);
       displaySearch(cities);
       getCordinats(city);
     } else if (city !== cities[cities.length - 1]) {
       getCordinats(city);
     }
+    $("#input").val("");
   });
 
   function displaySearch(cities) {
-    var $citylist = $(".search");
+    $citylist.text(" ");
     for (var i = 0; i < cities.length; i++) {
       var listItem = $("<li>" + cities[i] + "</li>");
+      $citylist.append(listItem);
     }
-    $citylist.append(listItem);
   }
+
+  $citylist.on("click", function (event) {
+    cityevent = event.target.textContent;
+    $("#citys").text(cityevent);
+    $("#input").val("");
+    getCordinats(cityevent);
+  });
+
   function getCordinats(city) {
     var cityname = city.toLowerCase();
 
@@ -62,14 +79,36 @@ $(document).ready(function () {
       url: currentUrl,
       method: "GET",
     }).then(function (response) {
-      console.log(response);
-      $(".temp").text(response.current.temp);
-      $(".humidity").text(response.current.humidity);
-      $(".wind").text(response.current.wind_speed);
-      $(".uv").text(response.current.uvi);
+      $("#icon").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" + response.current.weather[0].icon+ ".png"
+      );
+      $("#icon").attr("alt","the weathe icon");
+      $("#temp").text(response.current.temp);
+      $("#humidity").text(response.current.humidity);
+      $("#wind").text(response.current.wind_speed);
+      $("#uv").text(response.current.uvi);
+
+      setUVcolor(response.current.uvi);
 
       futureForcast(response);
     });
+  }
+
+  function setUVcolor(data){
+    if(data >= 0 && data <= 2)
+    {
+      $("#uv").css({"background-color":"green"});
+    }else if(data >= 3 && data <= 5)
+    {
+      $("#uv").css({"background-color":"yellow"});
+    }else if(data >= 6 && data <= 7)
+    {
+      $("#uv").css({"background-color":"orange"});
+    }else if(data >= 8 && data <= 10)
+    {
+      $("#uv").css({"background-color":"red"});
+    }
   }
 
   function futureForcast(data) {
@@ -84,10 +123,11 @@ $(document).ready(function () {
             .format("dddd, MMMM Do YYYY") +
           "</p>"
       );
+      var $img = $("<img src = http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png />");
       var $temp = $("<p>" + data.daily[i].temp.day + "</p>");
       var $humidity = $("<p>" + data.daily[i].humidity + "</p>");
 
-      $forcast.append($date, $temp, $humidity);
+      $forcast.append($date, $img, $temp, $humidity);
       $section.append($forcast);
     }
   }
